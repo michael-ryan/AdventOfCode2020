3725e2e6e12adee2bcdcd05fe0025125a048577c
mehmenmike <mehmenmike@gmail.com>
2020-12-02 18:52:04 +0000

    Use stream coming in from input file properly

--- src/Day2/GenericSolution.java
+++ src/Day2/GenericSolution.java
@@ -1,82 +1,49 @@
 package Day2;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Generic abstract class that implements annoying boilerplate parsing and stuff.
  */
 public abstract class GenericSolution {
     private static final File f = new File("src/Day2/data.txt");
-    private Entry[] entries;
-
-    /**
-     * Loads the {@link File} <code>f</code> variable and returns it as an array of integers.
-     * @return the content of <code>f</code>
-     */
-    protected void parseProblem(){
-        BufferedReader reader;
-        try{
-            reader = new BufferedReader(new FileReader(f));
-        } catch(FileNotFoundException e){
-            System.out.println("File " + f.toString() + " not found.");
-            System.exit(1);
-            return;
-        }
-
-        List<Entry> entryList = new ArrayList<>();
-        String s;
-        while (true){
-            try {
-                if((s = reader.readLine()) == null) break;
-            } catch(IOException e) {
-                e.printStackTrace();
-                System.exit(2);
-                return;
-            }
-            String[] splitData = s.split(" ");
-            int minimum = Integer.parseInt(splitData[0].split("-")[0]);
-            int maximum = Integer.parseInt(splitData[0].split("-")[1]);
-            char targetChar = splitData[1].charAt(0);
-            String password = splitData[2];
-            entryList.add(new Entry(minimum, maximum, targetChar, password));
-        }
-
-        entries = listToArray(entryList);
-    }
-
-    /**
-     * Filth to convert a {@link List} to an int array.
-     * @param entries the list to convert
-     * @return the content of the list in array form
-     */
-    private static Entry[] listToArray(List<Entry> entries)
-    {
-        Entry[] ret = new Entry[entries.size()];
-        Iterator<Entry> iterator = entries.iterator();
-        for (int i = 0; i < ret.length; i++)
-        {
-            ret[i] = iterator.next();
-        }
-        return ret;
-    }
+    private Stream<String> inputLines;
 
     /**
      * Finds the number of valid entries in an array.
      * @return the number of valid entries
      */
-    public int solve(){
+    public long solve(){
         this.parseProblem();
 
-        int numberCorrect = 0;
-        for(Entry entry : entries){
-            if(this.validateEntry(entry)){
-                numberCorrect++;
-            }
+        return this.inputLines
+                .map(GenericSolution::parseLine)
+                .filter(this::validateEntry)
+                .count();
+    }
+
+    /**
+     * Loads the {@link File} <code>f</code> variable and returns it as an array of integers.
+     */
+    private void parseProblem(){
+        BufferedReader reader;
+        try{
+            reader = new BufferedReader(new FileReader(f));
+        } catch(FileNotFoundException e){
+            throw new RuntimeException(e);
         }
-        return numberCorrect;
+
+        this.inputLines = reader.lines();
+    }
+
+    private static Entry parseLine(String s){
+        String[] splitData = s.split(" ");
+        int minimum = Integer.parseInt(splitData[0].split("-")[0]);
+        int maximum = Integer.parseInt(splitData[0].split("-")[1]);
+        char targetChar = splitData[1].charAt(0);
+        String password = splitData[2];
+        return new Entry(minimum, maximum, targetChar, password);
     }
 
     /**
