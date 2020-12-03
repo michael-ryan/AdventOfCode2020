a1e083add5ad9317ebc38327636e492636cff959
mehmenmike <mehmenmike@gmail.com>
2020-12-03 17:16:31 +0000

    Remove long line

--- src/Day3/Puzzle2/Solution.java
+++ src/Day3/Puzzle2/Solution.java
@@ -7,7 +7,13 @@ import java.util.stream.Stream;
 
 public class Solution extends GenericSolution {
     public static void main(String[] args){
-        Stream<Point> steps = Stream.of(new Point(1, 1), new Point(3, 1), new Point(5, 1), new Point(7, 1), new Point(1, 2));
+        Stream<Point> steps = Stream.of(
+                new Point(1, 1),
+                new Point(3, 1),
+                new Point(5, 1),
+                new Point(7, 1),
+                new Point(1, 2)
+        );
 
         GenericSolution solution = new Solution();
 
